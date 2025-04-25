ff111628cf5f724a98314487de9c91166add4ec2
[hadiaalv] <hadiaalvi18@gmail.com>
2025-04-25 14:49:05 +0500

    temp and humidity is displaying through  mqtt(snapshot)

--- labs/Lab10/1-sub_mqtt.py
+++ labs/Lab10/1-sub_mqtt.py
@@ -5,11 +5,36 @@ import ujson
 from machine import Pin, SoftI2C
 import ssd1306
 from umqtt.simple import MQTTClient
+import network
+
+
+#wifi configuration
+
+WIFI_SSID = 'NTU FSD'
+WIFI_PASS = ''
+
+
+print(f"Connecting to WiFi network '{WIFI_SSID}'")
+wifi = network.WLAN(network.STA_IF)
+wifi.active(True)
+wifi.connect(WIFI_SSID, WIFI_PASS)
+
+timeout = 10  # 10 seconds max wait 
+while not wifi.isconnected() and timeout > 0:
+    time.sleep(1)
+    print('WiFi connect retry ...')
+    timeout -= 1
+
+# checking wifi onnected or not
+if wifi.isconnected():
+    print('WiFi Connected! IP:', wifi.ifconfig()[0])
+else:
+    print('Failed to connect to WiFi. Check credentials or network.')
 
 # Configuration
 THINGSPEAK_MQTT_HOST = "mqtt3.thingspeak.com"
 THINGSPEAK_MQTT_PORT = 1883
-THINGSPEAK_CHANNEL_ID = "2923687"
+THINGSPEAK_CHANNEL_ID = "2924705"
 THINGSPEAK_CLIENT_ID = "JwYBGhwrLS48LCMIKSgvMSY"
 THINGSPEAK_USERNAME = "JwYBGhwrLS48LCMIKSgvMSY"
 THINGSPEAK_MQTT_API_KEY = "40a9cvyk92kUTeMDnjiS8t5S"
